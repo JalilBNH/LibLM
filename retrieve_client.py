15819546108d708253b16c1d74b74fae6badd8d8
Jalil <jalil.bnh13@gmail.com>
2026-04-28 10:09:53 +0200

    rm comment

--- retrieve_client.py
+++ retrieve_client.py
@@ -5,7 +5,7 @@ client = chromadb.PersistentClient('./chroma-db/')
 
 collection = client.get_collection(name='article-collection')
 
-query = 'What is the best NeRF architecture for semantic'
+query = 'How does NeRF works ?'
 
 results = collection.query(
     query_texts=query,
@@ -13,13 +13,6 @@ results = collection.query(
 )
 
 print('\n',query, '\n')
-
-# for doc, meta, distance in zip(
-#     results["documents"][0],
-#     results["metadatas"][0],
-#     results["distances"][0],
-# ):
-#     print(f"[{meta['source']}] (distance: {distance:.3f})\n{doc}\n")
 
 if results["documents"] and results["metadatas"] and results["distances"] is not None:
     for doc, meta, dist in zip(results["documents"][0], results["metadatas"][0], results["distances"][0]):
